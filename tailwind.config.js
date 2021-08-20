11dd3318eecb4fc05ee3052c08ab17f8aaa33776
danphranakorn13 <s6003051613133@gmail.com>
2021-08-20 13:56:58 +0700

    test upload to github

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.{html,js,php,blade.php}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
